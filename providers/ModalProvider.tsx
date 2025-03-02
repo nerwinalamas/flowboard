f8fd66aedf8f3b16534c184b83a7a4b3c0db1d51
Nerwin Alamas <nerwinalamas@gmail.com>
2025-03-02 16:36:22 +0800

    feat: modal provider

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import CreateTask from "@/components/modals/create-task";
+
+const ModalProvider = () => {
+  return (
+    <>
+      <CreateTask />
+    </>
+  );
+};
+
+export default ModalProvider;
