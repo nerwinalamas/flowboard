4270a88e8c5020d37ef44ce96ae966c74d922a18
Nerwin Alamas <nerwinalamas@gmail.com>
2025-03-09 14:29:06 +0800

    refactor: add editTask modal

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import CreateTask from "@/components/modals/create-task";
+import EditTask from "@/components/modals/edit-task";
 
 const ModalProvider = () => {
   return (
     <>
       <CreateTask />
+      <EditTask />
     </>
   );
 };
