9ebfd32f70e3daf957d7d8f01681dde9e0dc7265
Nerwin Alamas <nerwinalamas@gmail.com>
2025-03-16 10:55:52 +0800

    refactor: update schema

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 
-export const formSchema = z.object({
+export const taskSchema = z.object({
   title: z.string().min(1, "Title is required"),
   description: z.string().min(1, "Description is required"),
   priority: z.enum(["low", "medium", "high"]),
 });
+
+export type taskData = z.infer<typeof taskSchema>
