65fcafb9aa7fcc8924d9a73ae2a5167e0bdfafb4
Nerwin Alamas <nerwinalamas@gmail.com>
2025-04-05 12:15:15 +0800

    refactor: replace kanban buttons to theme toggle button

--- components/kanban-navbar.tsx
+++ components/kanban-navbar.tsx
@@ -1,11 +1,11 @@
 import FlowboardLogo from "./flowboard-logo";
-import KanbanAddButtons from "./kanban-add-buttons";
+import { ModeToggle } from "./mode-toggle";
 
 const KanbanNavbar = () => {
   return (
     <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between p-4">
       <FlowboardLogo />
-      <KanbanAddButtons />
+      <ModeToggle />
     </div>
   );
 };
