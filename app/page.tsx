dfcc0f22ae7ea171767767696a34b5a773d5be10
Nerwin Alamas <nerwinalamas@gmail.com>
2025-04-06 18:38:56 +0800

    refactor: add kanban filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import KanbanBoard from "@/components/kanban-board";
+import KanbanFilter from "@/components/kanban-filter";
 import KanbanNavbar from "@/components/kanban-navbar";
 
 const Home = () => {
   return (
     <main className="w-full h-screen mx-auto py-6 px-4 flex flex-col">
       <KanbanNavbar />
+      <KanbanFilter />
       <div className="flex-1 overflow-x-auto">
         <KanbanBoard />
       </div>
