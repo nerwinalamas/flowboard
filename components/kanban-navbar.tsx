5399a7fb2787743dd7ea6024cebf44879c610ca1
Nerwin Alamas <nerwinalamas@gmail.com>
2025-04-06 18:41:03 +0800

    refactor: update navbar layout

--- components/kanban-navbar.tsx
+++ components/kanban-navbar.tsx
@@ -1,11 +1,17 @@
 import FlowboardLogo from "./flowboard-logo";
+import KanbanAdd from "./kanban-add";
+import KanbanSettings from "./kanban-settings";
 import { ModeToggle } from "./mode-toggle";
 
 const KanbanNavbar = () => {
   return (
     <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between p-4">
       <FlowboardLogo />
-      <ModeToggle />
+      <div className="flex items-center gap-2">
+        <KanbanAdd />
+        <KanbanSettings />
+        <ModeToggle />
+      </div>
     </div>
   );
 };
