963f2bb1e55a4ac848a4840d95563b6d8e9d42e5
Nerwin Alamas <nerwinalamas@gmail.com>
2025-04-06 18:37:35 +0800

    feat: kanban add

---
+++ components/kanban-add.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useTaskModal } from "@/hooks/useTaskModal";
+import { useColumnModal } from "@/hooks/useColumnModal";
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const KanbanAdd = () => {
+  const { onOpen: onTaskModalOpen } = useTaskModal();
+  const { onOpen: onColumnModalOpen } = useColumnModal();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Plus className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => onColumnModalOpen("createColumn")}>
+          Column
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => onTaskModalOpen("createTask", "todo")}>
+          Task
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default KanbanAdd;
