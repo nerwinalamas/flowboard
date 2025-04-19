e3ea49ec7a10aa38e09dd3bba7c6362de774d1eb
Nerwin Alamas <nerwinalamas@gmail.com>
2025-04-19 10:36:28 +0800

    refactor: create new method for removing user and moved the sample user data to utils

--- hooks/useUserStore.ts
+++ hooks/useUserStore.ts
@@ -1,31 +1,18 @@
 import { create } from "zustand";
 import { User } from "@/lib/schema";
+import { sampleUsers } from "@/lib/data";
 
 interface UserState {
   users: User[];
   addUser: (user: User) => void;
+  removeUser: (userId: string) => void;
 }
-
-export const sampleUsers: User[] = [
-  {
-    id: "1",
-    name: "John Doe",
-  },
-  {
-    id: "2",
-    name: "Jane Smith",
-  },
-  {
-    id: "3",
-    name: "Robert Johnson",
-  },
-  {
-    id: "4",
-    name: "Emily Davis",
-  },
-];
 
 export const useUserStore = create<UserState>((set) => ({
   users: sampleUsers,
   addUser: (user) => set((state) => ({ users: [...state.users, user] })),
+  removeUser: (userId) =>
+    set((state) => ({
+      users: state.users.filter((user) => user.id !== userId),
+    })),
 }));
